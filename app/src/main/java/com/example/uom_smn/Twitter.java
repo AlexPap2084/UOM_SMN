ae65ae354e96cba6db462cbdc22aa0b6e8c4eeec
dai18029 <dai18029@uom.edu.gr>
2020-12-28 16:13:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/uom_smn/Twitter.java
@@ -0,0 +1,16 @@
+package com.example.uom_smn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Twitter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter);
+    
+    }
+}
