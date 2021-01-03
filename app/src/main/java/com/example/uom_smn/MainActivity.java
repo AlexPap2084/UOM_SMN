0f8e1a3b0181a4ee80d149a1891e38be227d2403
dai18029 <dai18029@uom.edu.gr>
2021-01-03 22:24:34 +0200

    Ολοκλήρωση Post σε Facebook !!!

--- app/src/main/java/com/example/uom_smn/MainActivity.java
+++ app/src/main/java/com/example/uom_smn/MainActivity.java
@@ -5,20 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String EMAIL = "email";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_to_app);
+
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
         Button btn = (Button) findViewById(R.id.btntwitt);
         Button btnPost =  (Button)findViewById(R.id.btnPost);
+
+
         btn.setOnClickListener(new View.OnClickListener()
         {
             
@@ -37,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
     public void openTwitterSearch() {
 
@@ -45,6 +65,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openCreateAPost(){
         startActivity(new Intent(MainActivity.this , CreateAPost.class));
+    }
+    /*public void openFacebook(){
+        startActivity(new Intent(MainActivity.this , FacebookActivity.class));
+    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
